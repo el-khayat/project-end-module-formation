6a71bd6797d6a78fa0e30bb85dda502b28c85734
unknown <aminemak42@gmail.comcls>
2024-01-25 14:36:21 +0100

    crud operation for Formation

--- frontend/src/pages/home/homePage.jsx
+++ frontend/src/pages/home/homePage.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import NavBar from '../../components/navbar/navbarComponent'
+// HomeFormationsPage.js
+import React, { useState, useEffect } from 'react';
+import NavBar from '../../components/navbar/navbarComponent';
+import FormationService from '../../services/formationServices';
+import FormationTable from './FormationTable';
 
-const HomePage = () => {
+const HomeFormationsPage = () => {
+  const [formations, setFormations] = useState([]);
+
+  useEffect(() => {
+    FormationService.getAllFormationsNoToken()
+      .then(response => {
+        console.log('Formations data retrieved:', response);
+        setFormations(response);
+      })
+      .catch(error => {
+        console.error('Error fetching formations:', error);
+      });
+  }, []);
+
   return (
     <div>
-     <NavBar />
-      <h1> Welcome to home page</h1>
+      <NavBar />
+      <h1>Available Formations</h1>
+      <FormationTable formations={formations} enrollButton={true} />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomeFormationsPage;
