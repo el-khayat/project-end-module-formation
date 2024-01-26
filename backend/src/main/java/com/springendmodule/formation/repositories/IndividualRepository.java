e6b584a5fc4ffe94fcc91f588fdd9bb70745934c
Driss Benomar <benomard999@gmail.com>
2024-01-26 20:13:35 +0100

    update the method update indivi because we need if already exist the indi take his old formations and append new formations

--- backend/src/main/java/com/springendmodule/formation/repositories/IndividualRepository.java
+++ backend/src/main/java/com/springendmodule/formation/repositories/IndividualRepository.java
@@ -1,10 +1,15 @@
 package com.springendmodule.formation.repositories;
 
 import com.springendmodule.formation.entities.Individual;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IndividualRepository extends JpaRepository<Individual, Integer> {
+	
+	List<Individual> findByNameAndEmail(String name,String email);
 
 }
